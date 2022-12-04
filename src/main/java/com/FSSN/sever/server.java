6b1d57c8e714c63a941bef14e49d651e9d9870ec
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-04 19:29:33 +0900

    HTTP 2.0 서버 개발 완료

--- src/main/java/com/FSSN/sever/server.java
+++ src/main/java/com/FSSN/sever/server.java
@@ -1,11 +1,7 @@
 package com.FSSN.sever;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @RestController
-public class SeverApplication {
+public class server {
+
+	static final int portNumber = 8000;
 
 	public static void main(String[] args) {
 		// Create a server on port 8000
-		SpringApplication.run(SeverApplication.class, args);
-	}
-
-	// 멀티 커넥터 설정
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-		return tomcat;
-	}
-
-	// http 1.1 포트 번호 연결
-	private Connector createStandardConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setPort(8080);
-		return connector;
+		SpringApplication.run(server.class, args);
 	}
 
 	@GetMapping("/")
@@ -59,7 +42,8 @@ public class SeverApplication {
 		// 서버 구동 전 호출할 함수
 		@PostConstruct
 		public void printStartMessage() {
-			System.out.println("Serving on https://0.0.0.0:8000/");
+			String startMessage = String.format("Serving on https://localhost: %d/", portNumber);
+			System.out.println(startMessage);
 		}
 	}
 }
